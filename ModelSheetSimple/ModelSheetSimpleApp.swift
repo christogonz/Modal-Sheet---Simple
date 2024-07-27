75f1bc3d44fdcb0182b02dd9295ca95e1075eaa2
Chris Gonzalez <christogonz92@gmail.com>
2024-07-27 11:43:52 +0200

    Initial Commit

---
+++ ModelSheetSimple/ModelSheetSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelSheetSimpleApp.swift
+//  ModelSheetSimple
+//
+//  Created by Christopher Gonzalez on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct ModelSheetSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
